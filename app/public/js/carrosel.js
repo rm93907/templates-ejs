db8a4e66b1dacf58ef58446e6ac258fa32cabbc0
Gabriel Soares <rm93907@estudante.fieb.edu.br>
2025-08-01 11:53:08 -0300

    cont7

---
+++ app/public/js/carrosel.js
@@ -0,0 +1,13 @@
+let slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+ 
+function changeSlide() {
+  slides[currentIndex].classList.remove('active');
+ 
+  currentIndex = (currentIndex + 1) % slides.length;
+ 
+  slides[currentIndex].classList.add('active');
+}
+ 
+// Trocar de imagem a cada 5 segundos
+setInterval(changeSlide, 1500);
